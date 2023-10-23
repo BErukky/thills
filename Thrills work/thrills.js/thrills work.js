28a917b1b159c99b89c03104e62041874436da11
Kilasho Faruq <kilashofaruq124@gmail.com>
2023-10-23 15:08:30 -0700

    mune working fine now

--- Thrills work/thrills.js/thrills work.js
+++ Thrills work/thrills.js/thrills work.js
@@ -1,8 +1,12 @@
-var die = document.querySelector('.bar');
-var nav = document.querySelector('.shc');
+const die = document.querySelector('.bar');
+const close = document.getElementById('close')
+const nav = document.querySelector('.shc');
+
 
 die.addEventListener('click', () => {
-  nav.classList.toggle("close")
+  nav.classList.toggle("open")
 
 });
-
+close.addEventListener('click', () => {
+  nav.classList.remove("open")
+});
