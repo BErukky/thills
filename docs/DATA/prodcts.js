10505b36ba8b756aa825912db11daff653bfe44a
Lordclassic <kilashofaruq124@gmail.com>
2024-01-28 23:29:53 -0800

    Done

---
+++ docs/DATA/prodcts.js
@@ -0,0 +1,48 @@
+
+const prodcts =[{
+  image:'photo/b11afd08-92b2-4677-bc8e-fe77fa59ee0f.jpg',
+  nameofcompy:'Thrills Couure',
+  prname:'Swiss Dasmask Fabrics',
+rating:'<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i>',
+price:'8000 per yard',
+cart:'<i class="fas fa-shopping-cart cart"></i>' 
+},
+{
+  image:'photo/55720c07-9f90-4e74-bf47-b1a37ea28fba.jpg',
+  nameofcompy:'Thrills Couure',
+  prname:'Italian cashmere',
+rating:'<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i>',
+price:'5000 per yard',
+cart:'<i class="fas fa-shopping-cart cart"></i>'  
+},
+{
+  image:'photo/BDEU2030.JPG', 
+  nameofcompy:'Thrills Couure',
+  prname:'Suiting chairman',
+rating:'<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i>',
+price:'3800 per yard',
+cart:'<i class="fas fa-shopping-cart cart"></i>' 
+},
+{image:'photo/DBSW5432.JPG', 
+  nameofcompy:'Thrills Couure',
+  prname:'Feltex cotton',
+rating:'<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i>',
+price:'5000 per yard' ,
+cart:'<i class="fas fa-shopping-cart cart"></i>'
+},
+{
+  image:'photo/our.JPG', 
+  nameofcompy:'Thrills Couure',
+  prname:'Swiss cotton',
+rating:'<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i>',
+price:'5000 per yard',
+cart:'<i class="fas fa-shopping-cart cart"></i>'
+},
+{
+  image:'photo/WCPA6726.JPG', 
+  nameofcompy:'Thrills Couure',
+  prname:'Premium fabrics',
+rating:'<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i>',
+price:'68p00 per yard',
+cart:'<i class="fas fa-shopping-cart cart"></i>'
+}];
--- docs/thrills.js/thrills work.js
+++ docs/thrills.js/thrills work.js
@@ -7,69 +7,44 @@ const cartTotla =document.querySelector('.cart-numb')
 const cartContenItrm =document.querySelector('.cart-cont')
 const shopDOM =document.querySelector('.content')
 const boDy = document.querySelector('.product1')
-const cartOrder = document.querySelector('.cart')
+
 
 die.addEventListener('click', () => {
   nav.classList.toggle("open")
 
 });
 close.addEventListener('click', () => {
-  nav.classList.remove("open")
+  nav.classList.remove("open") 
 });
 
-cartOrder.addEventListener('click', () => {
-  if (cartOrder.innerHTML === '<i></i>') {
-    cartOrder.innerHTML = '+';
-
-  } else {
-    cartOrder.innerHTML = '<i></i>';
-  };
-});
-console.log(cartOrder)
 
 
+boDy.addEventListener("click", (event)=>{
+  event.preventDefault();
+})
 
-//upload the img to js
-const prodcts =[{
-  image:'photo/b11afd08-92b2-4677-bc8e-fe77fa59ee0f.jpg',
-  nameofcompy:'Thrills Couure',
-  prname:'Swiss Dasmask Fabrics',
-rating:'<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i>',
-price:8000 
-},
-{
-  image:'photo/55720c07-9f90-4e74-bf47-b1a37ea28fba.jpg',
-  nameofcompy:'Thrills Couure',
-  prname:'Italian cashmere',
-rating:'<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i>',
-price:5000 
-},
-{
-  image:'photo/BDEU2030.JPG', 
-  nameofcompy:'Thrills Couure',
-  prname:'Suiting chairman',
-rating:'<i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i>',
-price:3800 
-}];
+
+//upload the HTML to js
 
 
 let prodctsHTML='';
  
 
-
 prodcts.forEach((prodcts)=>{
   prodctsHTML+= 
-  `<div class="con">
+  `<div class="content">
+  <div class="con">
         <img src="${prodcts.image}">
         <div class="dir"> <span>${prodcts.nameofcompy}</span>
           <h5>${prodcts.prname}</h5>
           <div class="star">
           ${prodcts.rating}
           </div>
-          <h4>₦${prodcts.price} per yard</h4>
+          <h4>₦${prodcts.price} </h4>
         </div>
-        <a href="#"><i class="fas fa-shopping-cart cart"></i></a>
-      </div>  
+        <a href="#">${prodcts.cart}</i></a>
+      </div> 
+      </div>   
       `; 
 })
  console.log(prodctsHTML); 
